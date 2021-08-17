109cfc130ef3b1cf06ee1c08da2d0db48f5c86c6
caio cesar <caiocesar_01@hotmail.com>
2021-08-17 19:20:44 -0300

    slide em js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+if(window.SimpleSlide) {
+
+new SimpleSlide({
+  slide: "quote", // nome do atributo data-slide="principal"
+  time: 4000, // tempo de transição dos slides
+  //nav: true, // se deve ou não mostrar a navegação
+  //pauseOnHover: true, // pausa a transição automática
+ // auto: true, // se o slide deve passar automaticamente
+});
+
+new SimpleSlide ({
+  slide:"portfolio",
+  time:5000,
+  nav:true  
+});
+
+}
